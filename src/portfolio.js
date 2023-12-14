d497db2738be7f0934f6b4940c2ce3e2a61d7bab
tinshu48 <tinshu48@gmail.com>
2023-12-14 17:28:55 +0530

    Uploaded Portfolio

---
+++ src/portfolio.js
@@ -0,0 +1,364 @@
+
+const settings = {
+  isSplash: true, // Change this to true if you want to use the splash screen.
+  useCustomCursor: true, // Change this to false if you want the good'ol cursor
+  googleTrackingID: "UA-174238252-2",
+};
+
+//Home Page
+const greeting = {
+  title: "Hello,",
+  title2: "Aryan",
+  logo_name: "aryan.py()",
+  nickname: "aryan728",
+  full_name: "Aryan Kumar",
+  subTitle:
+    "I am always enthusiastic to learn new technologies. I am always trying to create a project what I learned. I am interested in Python programming, Web development, and many more...   ",
+  resumeLink:
+    "https://drive.google.com/file/d/1zDi-tyDg6SQ7skKwukf4Y_dfVqsMXQHP/view?usp=sharing",
+  mail: "mailto:kumararyan1929@gmail.com",
+};
+
+const socialMediaLinks = {
+  /* Your Social Media Link */
+  github: "https://github.com/Aryank728",
+  linkedin: "https://www.linkedin.com/in/aryank728",
+  gmail: "kumararyan1929@gmail.com",
+  gitlab: " ",
+  facebook: "https://www.facebook.com/aryank728/",
+  twitter: "https://twitter.com/_aryankumar",
+  instagram: "https://www.instagram.com/mr.aryan__kumar/",
+};
+
+const skills = {
+  data: [
+    {
+      title: "Software & Web Development",
+      fileName: "FullStackImg",
+      skills: [
+        "⚡ Building responsive website front end using ReactJS",
+        "⚡ Backend development using NodeJS and ExpressJS",
+        "⚡ Proficient in Python and Java",
+        "⚡ Specialized in crafting robust applications",
+        "⚡ Problem-solving skills for complex coding challenges",
+        "⚡ Experience in building scalable solutions",
+      ],
+      softwareSkills: [
+        {
+          skillName: "HTML5",
+          fontAwesomeClassname: "simple-icons:html5",
+          style: {
+            color: "#E34F26",
+          },
+        },
+        {
+          skillName: "CSS3",
+          fontAwesomeClassname: "fa-css3",
+          style: {
+            color: "#1572B6",
+          },
+        },
+        {
+          skillName: "JavaScript",
+          fontAwesomeClassname: "simple-icons:javascript",
+          style: {
+            backgroundColor: "#FFFFFF",
+            color: "#F7DF1E",
+          },
+        },
+        
+        {
+          skillName: "Java",
+          fontAwesomeClassname: "simple-icons:java",
+          style: {
+            color: "#f89820",
+          },
+        },
+        
+        {
+          skillName: "Visual Basic",
+          fontAwesomeClassname: "simple-icons:dot-net",
+          style: {
+            color: "#029FCE",
+          },
+        },
+        
+        {
+          skillName: "ReactJS",
+          fontAwesomeClassname: "simple-icons:react",
+          style: {
+            color: "#61DAFB",
+          },
+        },
+        {
+          skillName: "NextJS",
+          fontAwesomeClassname: "simple-icons:nextdotjs",
+          style: {
+            color: "#FFFFFF",
+            backgroundColor: "#1D1D1D",
+            borderRadius: "50%",
+          },
+        },
+        {
+          skillName: "NodeJS",
+          fontAwesomeClassname: "simple-icons:node-dot-js",
+          style: {
+            color: "#339933",
+          },
+        },
+        {
+          skillName: "MongoDB",
+          fontAwesomeClassname: "simple-icons:mongodb",
+          style: {
+            color: "#439743",
+          },
+        },
+        
+        {
+          skillName: "MySQL",
+          fontAwesomeClassname: "simple-icons:mysql",
+          style: {
+            color: "#4479A1",
+          },
+        },
+        {
+          skillName: "Netlify",
+          fontAwesomeClassname: "simple-icons:netlify",
+          style: {
+            color: "#38AFBB",
+          },
+        },
+        {
+          skillName: "Python",
+          fontAwesomeClassname: "ion-logo-python",
+          style: {
+            backgroundColor: "transparent",
+            color: "#3776AB",
+          },
+        },
+
+
+        {
+          skillName: "Git",
+          fontAwesomeClassname: "simple-icons:git",
+          style: {
+            color: "#E94E32",
+          },
+        },
+      ],
+    },
+
+   
+  ],
+};
+
+const degrees = {
+  degrees: [
+    {
+      title: "Bengal Institute of Technology",
+      subtitle: "Bachelor's of Technology",
+      logo_path: "b.png",
+      alt_name: "DAL",
+      duration: "Aug 2019 - June 2023",
+      descriptions: [
+        "CGPA: 8.6"
+      ],
+      website_link: "https://www.bitcollege.in/",
+    },
+    {
+      title: "Govt. High School",
+      subtitle: "Higher Education",
+      logo_path: "dal.jpeg",
+      alt_name: "SETI",
+      duration: "2017 - 2018",
+      descriptions: [
+        "Percentage: 68%"
+      ],
+      website_link: " ",
+    },
+    {
+      title: "Vinay Sr. Sec. School",
+      subtitle: "Secondary Education",
+      logo_path: "vjt.png",
+      alt_name: "SETI",
+      duration: "2015 - 2016",
+      descriptions: [
+        "Percentage: 70%"
+      ],
+      website_link: " ",
+    },
+  ],
+};
+
+const certifications = {
+  certifications: [
+    {
+      title: "SQL (Basics)",
+      subtitle: "HackerRank",
+      logo_path: "aws.png",
+      certificate_link:
+        "https://drive.google.com/file/d/1Hs0qSiXj-8dNxOxegezDv_vUKWZwT0kK/view?usp=sharing",
+      alt_name: "SQL",
+      color_code: "#ffc475",
+    },
+    {
+      title: "Python (Basics)",
+      subtitle: "HackerRank",
+      logo_path: "aws.png",
+      certificate_link:
+        "https://drive.google.com/file/d/16TMEb7WcJhfW4kNtkyYbVHIggJNy1tiA/view?usp=sharing",
+      alt_name: "Python",
+      color_code: "#ffc475",
+    },
+    {
+      title: "Toycathon-21",
+      subtitle: "Organized by Govt. of India",
+      logo_path: "ino.png",
+      certificate_link: "https://drive.google.com/file/d/1ukpL8egG-1sKF_wSZEoBkDOy__ZvI0Yj/view?usp=sharing",
+      alt_name: "Google",
+      color_code: "#ffc475",
+    },
+    {
+      title: "Innovate India Coding Championship",
+      subtitle: "Organized by AICTE",
+      logo_path: "google_logo.png",
+      certificate_link: "https://drive.google.com/file/d/1kj2s44guAljG-DlXzn42toWcB8msH9ps/view?usp=sharing",
+      alt_name: "Google",
+      color_code: "#1e70c1",
+    },
+    
+  ],
+};
+
+// Experience Page
+const experience = {
+  title: "Experience",
+  subtitle: "Work, Internship and Volunteership",
+  description:
+    "I'm an IT fresher who is working as an Software Developer intern. I have prior knowledge of programing languages as well as full stack development.",
+  header_image_path: "experience.svg",
+  sections: [
+    {
+      title: "Internships",
+      experiences: [
+        {
+          title: "Full Stack Developer",
+          company: "Helper4U",
+          company_url: "https://www.helper4u.in/",
+          logo_path: "cra.png",
+          duration: "Dec 2023 - Ongoing",
+          location: "Work From Home",
+          description:
+            "Helper4U helps you hire Helpers - Better, Faster, Safer. We use technology to connect employers with Housekeeping staff, Maids, Nanny, Cook, Driver, Caregivers & everyone similar, on their mobile, without the need of any agency or middleman",
+          color: "#ee3c26",
+        },
+      ]
+    },
+  ]
+};
+
+// Projects Page
+const projectsHeader = {
+  title: "Projects",
+  description:
+    "My projects make use of a vast variety of the latest technology tools. 🚀 I excel in creating both Web Development and Software Development projects. Below, you'll find some of my projects. Please note that not all mentioned projects are on GitHub yet. 🌐🛠️",
+  avatar_image_path: "projects_image.svg",
+};
+
+// Contact Page
+const contactPageData = {
+  contactSection: {
+    title: "Contact Me",
+    profile_image_path: "hrishi2.png",
+    description:
+      "You can contact me at the places mentioned below. I will try to get back to you as fast as I can. ",
+  },
+  blogSection: {
+    // title: "Blogs",
+    // subtitle:
+    //   "I don't blog frequently but when I do something awesome, I do try my best to write a blog about it.",
+    // link: "https://medium.com/@hrishipatel99",
+    avatar_image_path: "blogs_image.svg",
+  },
+};
+
+const projects = {
+  data: [
+    {
+      name: "Virtual Assistant",
+      url: "https://github.com/Aryank728/Virtual-Assistant",
+      description:
+        "This is a small virtual assistant for my laptop which can do small tasks. I made this virtual assistant in python ",
+      languages: [
+        {
+          name: "Python",
+          iconifyClass: "logos-python",
+        },
+      ],
+    },
+    {
+      name: "OneNote",
+      url: "https://onennote.netlify.app/",
+      description: "This is a online todo app which looks like sticky notes. It also has search feature by which user can search their task easily",
+      languages: [
+        {
+          name: "ReactJS",
+          iconifyClass: "logos-react",
+        },
+      ],
+    },
+    {
+      name: "Youtube Video Downloader",
+      url: "https://github.com/Aryank728/youtube-downloader",
+      description:
+        "This is a youtube downloader which I created using python.",
+      languages: [
+        {
+          name: "Python",
+          iconifyClass: "logos-python",
+        },
+      ],
+    },
+    {
+      name: "Pdf-to-Audio",
+      url: "https://github.com/Aryank728/Pdf-to-audio",
+      description:
+        "I created this pdf to audio convertoer using python. This takes a pdf at one time as a input and convert the content of pdf into audio and provide audio output Immediately",
+      languages: [
+        {
+          name: "Python",
+          iconifyClass: "logos-python",
+        },
+      ],
+    },
+    {
+      name: "Chat Whisper",
+      url: "https://github.com/Aryank728/chat-whisper",
+      description:
+        "This is a small realtime chatting web app which is used for chatting to friends. You can also send pictures in this.",
+      languages: [
+        {
+          name: "ReactJS",
+          iconifyClass: "logos-react",
+        },
+        {
+          name: "JavaScript",
+          iconifyClass: "logos-javascript",
+        },
+      ],
+    },
+  ],
+};
+
+export {
+  settings,
+  greeting,
+  socialMediaLinks,
+  skills,
+  degrees,
+  certifications,
+  experience,
+  projectsHeader,
+  contactPageData,
+  projects,
+};
