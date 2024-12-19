bc3b7b30dbb33fc50e0ff6caa67fd2045a5062c0
tinshu48 <tinshu48@gmail.com>
2024-12-19 08:19:30 +0530

    Added jobs sections

--- src/portfolio.js
+++ src/portfolio.js
@@ -241,6 +241,13 @@ const skills = {
             color: "#E94E32",
           },
         },
+        {
+          skillName: "Azure",
+          fontAwesomeClassname: "simple-icons:microsoftazure",
+          style: {
+            color: "#38AFBB",
+          },
+        },
       ],
     },
   ],
@@ -344,6 +351,44 @@ const experience = {
           ⚡ Utilized problem-solving skills to troubleshoot and resolve technical issues promptly. \n
           ⚡ Consistently delivered high-quality deliverables within strict deadlines, fostering client satisfaction. \n
           ⚡ Utilized HTML, CSS, Tailwind CSS, Node.js, React.js, Express.js, Next.js, Knex.js & MySQL within the project’s Tech Stack.`,
+          color: "#ee3c26",
+        },
+      ],
+    },
+    {
+      title: "JOBS",
+      experiences: [
+        {
+          title: "Software Engineer",
+          company: "LTIMindtree",
+          company_url: "https://www.ltimindtree.com/",
+          logo_path: "lti.jpg",
+          duration: "Sept 2024 - Present",
+          location: "On-site",
+          description: `
+          ⚡ Utilizing JavaScript (JS) to create dynamic, responsive, and interactive web applications.
+          ⚡ Managing and optimizing databases with MongoDB and MySQL, ensuring efficient data handling.
+          ⚡ Deploying and managing cloud-based applications using Azure, enhancing operational efficiency.
+          ⚡ Implementing virtualization solutions and system optimization through VMWare.
+          ⚡ Automating repetitive tasks and improving system performance using PowerShell scripting.
+          ⚡ Applying networking concepts to streamline application connectivity and ensure robust system integration.`,
+          color: "#ee3c26",
+        },
+        {
+          title: "Graduate Engineer Trainee",
+          company: "LTIMindtree",
+          company_url: "https://www.ltimindtree.com/",
+          logo_path: "lti.jpg",
+          duration: "Jun 2024 - Sept 2024",
+          location: "On-site",
+          description: `
+          ⚡ Gained foundational knowledge and training in HTML, CSS, and Java, enabling efficient web development practices.
+          ⚡ Acquired hands-on training in JavaScript (JS) for creating interactive user experiences.
+          ⚡ Completed training in C#, focusing on object-oriented programming principles.
+          ⚡ Developed a strong understanding of database management systems like MongoDB and MySQL.
+          ⚡ Enhanced skills in virtualization technology with VMWare training.
+          ⚡ Learned the fundamentals of cloud computing and deployment through Azure training.
+          ⚡ Mastered the basics of scripting and automation using PowerShell during training sessions.`,
           color: "#ee3c26",
         },
       ],
